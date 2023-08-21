998a0b26481fa608b84f169cd81edb42c885181f
lato99 <72751305+lato99@users.noreply.github.com>
2023-08-21 13:51:14 +0300

    Add files via upload

---
+++ Models/person_user.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace PortfolioMakerApp.Models
+{
+	public class person_user
+	{
+        
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string age { get; set; }
+        public string address { get; set; }
+        public string martialStatus { get; set; }
+        public string telephone { get; set; }
+        public string email { get; set; }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        }
+    }
+
+
+}
+
