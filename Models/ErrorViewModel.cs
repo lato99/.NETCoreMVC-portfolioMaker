b5b18db7d40652198ab2cd5d0d973f6d016f6696
lato99 <72751305+lato99@users.noreply.github.com>
2023-08-21 13:58:40 +0300

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PortfolioMakerApp.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
